cbfbfef3d2069c12dcabd69c9ae776e145ea8903
Jpadilla1 <jpadilla1293@gmail.com>
2016-04-20 13:20:47 -0400

    add app icon & splashscreen & redux remote devtools

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,12 +1,18 @@
 import { AsyncStorage } from 'react-native';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
+import devTools from 'remote-redux-devtools';
 import { persistStore, autoRehydrate } from 'redux-persist';
 
 import * as reducers from '../reducers';
 
+const devToolsConfig = {
+  hostname: 'localhost',
+  port: 8000
+};
+
 const isDebuggingInChrome = __DEV__ && !!window.navigator.userAgent;
-const createFSNStore = applyMiddleware(thunk)(createStore);
+const createFSNStore = compose(applyMiddleware(thunk), devTools(devToolsConfig))(createStore);
 
 function configureStore(onComplete) {
   const reducer = combineReducers(reducers);
