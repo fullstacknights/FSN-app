18c6198bad7a4043782c120f0deb64062635759c
Emmanuel Luciano <emma.luciano11@gmail.com>
2016-05-10 16:29:01 -0400

    Adds Montserrat syntax for Android and for iOS

--- app/utils/styleHelpers.js
+++ app/utils/styleHelpers.js
@@ -1,7 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   montserratText: {
-    fontFamily: 'Montserrat'
+    fontFamily: (Platform.OS === 'ios') ? 'Montserrat' : 'montserrat'
   }
 })
