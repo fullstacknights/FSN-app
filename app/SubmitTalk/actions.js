379efd09011415b9d05bddc44082a658e392797b
Emmanuel Luciano <emma.luciano11@gmail.com>
2016-05-11 14:07:20 -0400

    Changes action types names.

--- app/SubmitTalk/actions.js
+++ app/SubmitTalk/actions.js
@@ -1,40 +1,41 @@
 import * as actionTypes from './actionTypes';
 import submit from '../api/postData';
+import { handleDisplayAlert } from '../Alert/actions';
 
 export function handleAddName(event) {
-  return ({type: actionTypes.ADD_NAME, name: event.nativeEvent.text});
+  return ({type: actionTypes.TALK_NAME, name: event.nativeEvent.text});
 }
 
 export function handleAddEmail(event) {
-  return ({type: actionTypes.ADD_EMAIL, email: event.nativeEvent.text});
+  return ({type: actionTypes.TALK_EMAIL, email: event.nativeEvent.text});
 }
 
 export function handleAddOnlineProfile(event) {
-  return ({type: actionTypes.ADD_ONLINE_PROFILE, onlineProfile: event.nativeEvent.text});
+  return ({type: actionTypes.TALK_ONLINE_PROFILE, onlineProfile: event.nativeEvent.text});
 }
 
 export function handleAddProfession(profession) {
-  return ({type: actionTypes.ADD_PROFESSION, profession});
+  return ({type: actionTypes.TALK_PROFESSION, profession});
 }
 
 export function handleAddTopic(event) {
-  return ({type: actionTypes.ADD_TOPIC, topic: event.nativeEvent.text});
+  return ({type: actionTypes.TALK_TOPIC, topic: event.nativeEvent.text});
 }
 
 export function handleAddImportance(event) {
-  return ({type: actionTypes.ADD_IMPORTANCE, importance: event.nativeEvent.text});
+  return ({type: actionTypes.TALK_IMPORTANCE, importance: event.nativeEvent.text});
 }
 
 export function handleAddQuestionsComments(questionsComments) {
-  return ({type: actionTypes.ADD_QUESTIONS_COMMENTS, questionsComments});
+  return ({type: actionTypes.TALK_QUESTIONS_COMMENTS, questionsComments});
 }
 
 export function handleIsLoading(isLoading) {
-  return ({type: actionTypes.IS_LOADING, isLoading});
+  return ({type: actionTypes.TALK_IS_LOADING, isLoading});
 }
 
-export function handleClearState() {
-  return ({type: actionTypes.CLEAR_STATE});
+export function handleClearSubmitTalk() {
+  return ({type: actionTypes.CLEAR_SUBMIT_TALK});
 }
 
 export function handleSubmit(data) {
@@ -42,7 +43,15 @@ export function handleSubmit(data) {
     dispatch(handleIsLoading(true));
     submit(data, 'classes/TalkSubmission')
       .then(() => {
-        dispatch(handleClearState());
+        const alert = {
+          style: 'success',
+          messageLine1: 'Thank you for submiting your talk!',
+          messageLine2: 'The FSN team will evaluate your proposal and get back to you.',
+          header: 'Success!'
+        };
+
+        dispatch(handleDisplayAlert(alert));
+        dispatch(handleClearSubmitTalk());
       });
   });
 }
--- app/SubmitTalk/reducer.js
+++ app/SubmitTalk/reducer.js
@@ -13,23 +13,23 @@ const initialState = {
 
 export default function openMic(state = initialState, action) {
   switch (action.type) {
-    case actionTypes.ADD_NAME:
+    case actionTypes.TALK_NAME:
       return (Object.assign({}, state, {name: action.name}));
-    case actionTypes.ADD_EMAIL:
+    case actionTypes.TALK_EMAIL:
       return (Object.assign({}, state, {email: action.email}));
-    case actionTypes.ADD_ONLINE_PROFILE:
+    case actionTypes.TALK_ONLINE_PROFILE:
       return (Object.assign({}, state, {onlineProfile: action.onlineProfile}));
-    case actionTypes.ADD_PROFESSION:
+    case actionTypes.TALK_PROFESSION:
       return (Object.assign({}, state, {profession: action.profession}));
-    case actionTypes.ADD_TOPIC:
+    case actionTypes.TALK_TOPIC:
       return (Object.assign({}, state, {topic: action.topic}));
-    case actionTypes.ADD_IMPORTANCE:
+    case actionTypes.TALK_IMPORTANCE:
       return (Object.assign({}, state, {importance: action.importance}));
-    case actionTypes.ADD_QUESTIONS_COMMENTS:
+    case actionTypes.TALK_QUESTIONS_COMMENTS:
       return (Object.assign({}, state, {questionsComments: action.questionsComments}));
-    case actionTypes.IS_LOADING:
+    case actionTypes.TALK_IS_LOADING:
       return (Object.assign({}, state, {isLoading: action.isLoading}));
-    case actionTypes.CLEAR_STATE:
+    case actionTypes.CLEAR_SUBMIT_TALK:
       return (Object.assign({}, state, initialState));
     default:
       return (state);
