209af8b82a1c252abadec943d6c7a98a9efc904a
Emmanuel Luciano <emma.luciano11@gmail.com>
2016-05-11 17:35:57 -0400

    Adds alert to Open Mic container.

--- app/OpenMic/Container.js
+++ app/OpenMic/Container.js
@@ -5,22 +5,38 @@ import { connect } from 'react-redux';
 import { PureRender } from '../components';
 import Form from './components/form/index';
 import * as OpenMicActions from './actions';
+import * as AlertActions from '../Alert/actions';
+import Alert from '../Alert/components/alert/index';
 
 class OpenMic extends Component {
   render() {
-    return (<Form openMic={this.props.openMic} actions={this.props.openMicActions}/>);
+    if (this.props.alert.display) {
+      return (
+        <Alert {...this.props.alert} actions={this.props.alertActions} />
+      );
+    }
+
+    return (
+      <Form
+        openMic={this.props.openMic}
+        actions={this.props.openMicActions}
+        alert={this.props.alert}
+        alertActions={this.props.alertActions} />
+    );
   }
 }
 
 function setupState(state) {
   return ({
-    openMic: state.openMic
+    openMic: state.openMic,
+    alert: state.alert
   });
 }
 
 function setupActions(dispatch) {
   return ({
-    openMicActions: bindActionCreators(OpenMicActions, dispatch)
+    openMicActions: bindActionCreators(OpenMicActions, dispatch),
+    alertActions: bindActionCreators(AlertActions, dispatch)
   });
 }
 
