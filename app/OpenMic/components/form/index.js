b8cef75a82044d8043b373fff0dc715629b0837e
Jpadilla1 <jpadilla1293@gmail.com>
2016-05-11 11:36:07 -0400

    small changes

--- app/OpenMic/components/form/index.js
+++ app/OpenMic/components/form/index.js
@@ -5,7 +5,10 @@ import React, {
   View,
   Text,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  DeviceEventEmitter,
+  Dimensions,
+  Platform
 } from 'react-native';
 import Spinner from 'react-native-spinkit';
 
@@ -16,57 +19,84 @@ import helpers from '../../../utils/styleHelpers';
 export default class Form extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      focusedInput: '',
+      visibleHeight: Dimensions.get('window').height,
+      keyboardShowing: false
+    };
     this.scrollTo = this.scrollTo.bind(this);
   }
+  componentWillMount() {
+    DeviceEventEmitter.addListener(Platform.OS == 'IOS' ? 'keyboardWillShow' : 'keyboardDidShow', this.keyboardWillShow.bind(this))
+    DeviceEventEmitter.addListener(Platform.OS == 'IOS' ? 'keyboardWillHide' : 'keyboardDidHide', this.keyboardWillHide.bind(this))
+  }
+  keyboardWillShow (e) {
+    const newSize = Dimensions.get('window').height - e.endCoordinates.height;
+    this.setState({ visibleHeight: newSize, keyboardShowing: true });
+  }
+
+  keyboardWillHide (e) {
+    this.setState({ visibleHeight: Dimensions.get('window').height, keyboardShowing: false });
+  }
+
   scrollTo(input) {
-    setTimeout(() => {
-      this.refs[input].measure((ox, oy, width, height, px, py) => {
-        this._scrollView.scrollTo({ x: ox, y: oy, animation: true });
+    return () => {
+      this.refs[input].focus();
+      setTimeout(() => {
+        if (this.state.focusedInput !== input) {
+          this.refs[input].measure((ox, oy) => {
+            this._scrollView.scrollTo({ x: 0, y: oy - 30, animation: true });
+          });
+          this.setState({ focusedInput: input });
+        }
       });
-    })
+    };
   }
   renderForm() {
     const buttonState = (this.props.openMic.isLoading) ? styles.openMicDisabled : styles.openMic;
+    const scrollViewContent = {
+      paddingBottom: this.state.keyboardShowing ? this.state.visibleHeight - 150 : 0
+    };
     return (
       <ScrollView
         ref={(sv) => this._scrollView = sv}
-        contentContainerStyle={[styles.container, styles.scrollView]}
+        style={[styles.container, styles.scrollView]}
+        contentContainerStyle={scrollViewContent}
       >
         <View style={styles.logoWrapper}>
           <Image
             style={styles.logo}
-            source={logo}/>
+            source={logo}
+          />
         </View>
         <Text style={[styles.whiteText, styles.centerText, styles.spaceBottom, helpers.montserratText]}>
           Open Mics have a limited time of 5 minutes. Slides are not allowed in the presentation. You may anounce or speak of whatever you like.  Please be respectful.
         </Text>
-        <View style={styles.spaceBottom}>
-          <Text style={[styles.whiteText, styles.boldText, helpers.montserratText]}>
-            What is your name?
-          </Text>
-          <TextInput
-            defaultValue={this.props.openMic.name}
-            ref='name'
-            style={styles.input}
-            autoCapitalize="words"
-            autoFocus={true}
-            autoCorrect={false}
-            onFocus={this.scrollTo('name')}
-            onBlur={this.props.actions.handleAddName}
-            />
-        </View>
-        <View style={styles.spaceBottom}>
-          <Text style={[styles.whiteText, styles.boldText, helpers.montserratText]}>
-            What is the topic?
-          </Text>
-          <TextInput
-            defaultValue={this.props.openMic.topic}
-            ref='topic'
-            style={styles.input}
-            onFocus={this.scrollTo('topic')}
-            onChangeText={this.props.actions.handleAddTopic}
+        <Text style={[styles.whiteText, styles.boldText, helpers.montserratText]}>
+          What is your name?
+        </Text>
+        <TextInput
+          defaultValue={this.props.openMic.name}
+          ref='name'
+          style={styles.input}
+          autoCapitalize="words"
+          autoCorrect={false}
+          onFocus={this.scrollTo('name')}
+          onSubmitEditing={this.scrollTo('topic')}
+          onBlur={this.props.actions.handleAddName}
           />
-        </View>
+        <View style={styles.spaceBottom}/>
+        <Text style={[styles.whiteText, styles.boldText, helpers.montserratText]}>
+          What is the topic?
+        </Text>
+        <TextInput
+          defaultValue={this.props.openMic.topic}
+          ref='topic'
+          style={styles.input}
+          onFocus={this.scrollTo('topic')}
+          onChangeText={this.props.actions.handleAddTopic}
+        />
+        <View style={styles.spaceBottom}/>
         <View style={styles.spaceBottom}>
           <TouchableOpacity disabled={this.props.openMic.isLoading} onPress={this.props.actions.handleSubmit.bind(null, this.props.openMic)}>
             <View style={[styles.button, buttonState]}>
